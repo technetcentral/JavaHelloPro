f19174a6c2ea676c71b1ed6b437ebfc90b941a8f
Alvin Vaughn <avaughn@vaughnsonline.net>
2016-04-21 21:45:31 -0500

    Update Test

--- HelloWorld/src/Application.java
+++ HelloWorld/src/Application.java
@@ -4,8 +4,10 @@ public class Application {
 	public static void main(String[] args) {
 		int myNumber;
 		 myNumber = 22;
+		 double percent = 3;
 		System.out.println("Hello World");
 		System.out.println("My Number is " + myNumber);
+		System.out.println("22 Divided by 3 is :" + myNumber/percent);
 	}
 
 }
