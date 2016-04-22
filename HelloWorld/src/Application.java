7d985a94f6f4f3e5c1e4f1e04ab4dcae295f0675
Alvin Vaughn <avaughn@vaughnsonline.net>
2016-04-21 21:37:54 -0500

    Committed Java Hello World

---
+++ HelloWorld/src/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber;
+		 myNumber = 22;
+		System.out.println("Hello World");
+		System.out.println("My Number is " + myNumber);
+	}
+
+}
